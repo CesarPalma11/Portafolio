bd0e38cd40a4e699ed02475ded207bcaa8ae8de5
CesarPalma11 <cesarpalmag16@gmail.com>
2024-06-23 16:34:45 -0300

    formulario contacto

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,74 +1,59 @@
-import React, { useState } from 'react'
-import transition from '../FramerMotion/transition'
-import './Contact.css'
+import React, { useRef, useState } from 'react';
+import transition from '../FramerMotion/transition';
+import emailjs from '@emailjs/browser';
+import './Contact.css';
+
 const Contact = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        message: ''
-      });
-    
-      const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value
-        });
-      };
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Aquí puedes manejar el envío del formulario, como enviar los datos a un servidor.
-        console.log('Formulario enviado', formData);
-        // Limpiar el formulario después de enviarlo
-        setFormData({
-          name: '',
-          email: '',
-          message: ''
-        });
-      };
+  const form = useRef();
+  const [popupVisible, setPopupVisible] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_gh81kgl', 'template_j7u5nfo', form.current, 'PoFy_VMBgwUQyAfCA')
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          form.current.reset();
+          setPopupVisible(true);
+          setTimeout(() => setPopupVisible(false), 3000);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        }
+      );
+  };
+
   return (
     <>
-         <div className="contact-form-container">
-      <form className="contact-form" onSubmit={handleSubmit}>
-        <h2>Contacto</h2>
-        <div className="form-group">
-          <label htmlFor="name">Nombre</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+      <div className="contact-form-container">
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <h2>Contacto</h2>
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input type="text" id="name" name="user_name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Correo Electrónico</label>
+            <input type="email" id="email" name="user_email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Mensaje</label>
+            <textarea id="message" name="message" required />
+          </div>
+          <button type="submit" value="Send" style={{ background: 'orange', fontWeight: 'bold' }}>
+            Hablemos!
+          </button>
+        </form>
+      </div>
+      {popupVisible && (
+        <div className="popup">
+          <p>¡Mensaje enviado con éxito!</p>
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Correo Electrónico</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="message">Mensaje</label>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" style={{background:'orange', fontWeight:'bold'}}>Hablemos!</button>
-      </form>
-    </div>
+      )}
     </>
-  )
-}
+  );
+};
 
 export default transition(Contact);
